3610ed9cf0ccea817b91d17d34f20466b1920f82
allexeyv <allexeyv@gmail.com>
2019-05-26 23:09:36 +0300

    added Category and extended User models

--- backend/pp_app/admin.py
+++ backend/pp_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserPP, Category
 
+
+admin.site.register(UserPP)
+admin.site.register(Category)
 # Register your models here.
--- backend/pp_app/models.py
+++ backend/pp_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class UserPP(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=30, blank=True, default='')
+    address = models.CharField(max_length=500, blank=True, default='')
+    #phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+    #                             message="Phone number must be entered in the format: '+x xxx xxx xx xx'. Up to 15 digits allowed.")
+    #phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=False)
+    phone_number = models.CharField(max_length=15, blank=False)
+    week_show_count = models.IntegerField(blank=True, default=0)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        ordering = ('user',)
+
+
+class Category(models.Model):
+    users = models.ManyToManyField(UserPP)
+    name = models.CharField(max_length=30, blank=True, default='')
+    description = models.CharField(max_length=30, blank=True, default='')
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+
+
